acc51d4bdb812cc6019ad289b0564b0d79ff3a9d
Gathsara2002 <liviru2002mmg@gmail.com>
2023-12-15 12:48:23 +0530

    Content updated with object destructuring

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-//Template literals
+//Template literals ------------------------------------------------------------------
 
-let name1='Gathsara';
-let name2='Liviru';
+let name1 = 'Gathsara';
+let name2 = 'Liviru';
 
-let name=`${name1} ${name2}`;
+let name = `${name1} ${name2}`;
 console.log(name);
 
-//
+
+//Object Destructuring ----------------------------------------------------------------
+
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    city: "LA",
+    detail : {
+        data: 'hi'
+    }
+}
+
+const { firstName, lastName, city } = person;
+console.log(firstName, lastName, city);
+
+//Use alias to rename variables
+const { firstName: fn } = person;
+console.log(fn);
+
+//Nested object
+const{detail:{data}}=person;
+console.log(data);
